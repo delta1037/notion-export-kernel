929c862306bcd206b7f62f3d3a7d9f37bdf17139
liubo <geniusrabbit@qq.com>
2022-01-12 08:35:46 +0800

    发布0.0.4版本

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ if "bdist_wheel" in sys.argv:
 
 REQUIRES = ["notion-client>=0.8.0"]
 
-with open("README_En.md.md", encoding="utf-8") as handle:
-    readme_rst = handle.read()
+# with open("README_En.md", encoding="utf-8") as handle:
+#    readme_rst = handle.read()
 
 setup(
     name="notion-dump-kernel",
@@ -32,7 +32,7 @@ setup(
     author_email="geniusrabbit@qq.com",
     url="https://github.com/delta1037/notion-dump-kernel",
     description="Freely available tools for dumping Notion page and database.",
-    long_description=readme_rst,
+    # long_description=readme_rst,
     project_urls={
         "Documentation": "https://github.com/delta1037/notion-dump-kernel/blob/main/README_En.md",
         "Source": "https://github.com/delta1037/notion-dump-kernel",
@@ -52,3 +52,7 @@ setup(
     include_package_data=True,  # done via MANIFEST.in under setuptools
     install_requires=REQUIRES,
 )
+# 打包发布
+# 1、python setup.py sdist
+# 2、twine upload dist/*
+
