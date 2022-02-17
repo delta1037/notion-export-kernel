7c1208325e2337ffe227a301578d7d20d711824d
liubo <geniusrabbit@qq.com>
2022-02-17 11:56:42 +0800

    发布0.1.7版本

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ REQUIRES = ["notion-client>=0.8.0"]
 
 setup(
     name="notion-dump-kernel",
-    version="0.1.6",
+    version="0.1.7",
     author="delta1037",
     author_email="geniusrabbit@qq.com",
     url="https://github.com/delta1037/notion-dump-kernel",
